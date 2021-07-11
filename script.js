3ec2e3b1e6c496226df6955ae47e1562358e8c17
CEED87 <ceed210787@gmail.com>
2021-07-11 14:57:23 +0300

    add_forms_in_HTML

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 /*1. Объявить класс под названием “Book”.
 2. Класс “Book”. У каждой книги есть “название”, “автор”, “id”, “количество страниц”.
 3. Класс “TravelBook”. У каждой новой книги есть: “название”, “автор”, “id”, “количество страниц”,
- “тип обложки”. Должен быть метод который выводит информацию о “названии”, “авторе”, “количестве страниц”. Должен быть метод удаления книги из списка.
+ “тип обложки”. Должен быть метод который выводит информацию о “названии”, “авторе”,
+  “количестве страниц”. Должен быть метод удаления книги из списка.
 3. Класс “Comics”. У каждой новой книги есть: “название”, “автор”, “id”, “количество страниц”, “номер выпуска”. Должен быть метод который выводит информацию о “названии”, “авторе”, “количестве страниц”. Должен быть метод удаления книги из списка.
 4. Внешний интерфейс должен быть следующим:
 Слева все поля вводакоторые отвечают за добавление новой книги от класса TravelBook;
@@ -22,8 +23,8 @@ class Book {
 }
 
 class TravelBook extends Book {
-    constructor(covType) {
-        super();
+    constructor(name, author, id, countPages, covType) {
+        super(name, author, id, countPages);
         this._covType = covType;
     }
     get info() {
@@ -34,9 +35,15 @@ class TravelBook extends Book {
     }
 }
 
-class Comics extends Book {
-    constructor(num) {
-        super();
-        this._num = num;
+class Comics extends TravelBook {
+    constructor(name, author, id, countPages, num) {
+        super(name, author, id, countPages);
+        this.num = num;
     }
+
 }
+
+const travelBook = new TravelBook('название', 'автор', 'id', 'количество страниц','тип обложки');
+const comics = new Comics('название2', 'автор2', 'id2', 'количество страниц2', 3);
+
+// console.log(comics.num)
